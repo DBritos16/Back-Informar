886507536dc1d7cdd0a8a42db336d07957ba6280
DBritos16 <dbriitos16@gmail.com>
2022-12-03 00:11:04 -0300

    Logica para filtrado implementada 02-12-22

--- src/controllers/carreras.controllers.js
+++ src/controllers/carreras.controllers.js
@@ -2,28 +2,58 @@ const carreraSchema = require('../models/carreras.model')
 const ctrl = {};
 
 
-ctrl.getCarrera = async (req, res)=>{
+ctrl.getCarrera = async (req, res) => {
+
+    const filtros = req.body
+    const { limite, texto } = req.headers
+
+    for (const key in filtros) {
+        if (filtros[key].length === 0) {
+            delete filtros[key]
+        }
+    }
+
+    console.log(filtros);
 
     try {
-        const getCarreras = await carreraSchema.find({isActive: true});
-
-        res.json(getCarreras);
+        if (texto) {
+            carreraSchema.find(filtros)
+                .find({ nombre: { $regex: texto, $options: "i" } })
+                .find({isActive: true})
+                .sort({updatedAt: -1})
+                .limit(limite)
+                .exec((err, carreras) => {
+                    if (err) return res.status(400).json({ msg: 'Ha ocurrido un error' })
+                    return res.status(200).json(carreras)
+                })
+        } else {
+            carreraSchema.find(filtros)
+                .find({isActive: true})
+                .sort({updatedAt: -1})
+                .limit(limite)
+                .exec((err, carreras) => {
+                    if (err) return res.status(400).json({ msg: 'Ha ocurrido un error' })
+                    return res.status(200).json(carreras)
+                })
+        }
 
     } catch (error) {
-        res.json(`Ha ocurrido un error: ${error}`)
+        res.json({
+            msg: 'Ha ocurrido un error'
+        })
     }
 
 }
 
 
-ctrl.getInfoCarrera = async (req, res)=>{
+ctrl.getInfoCarrera = async (req, res) => {
 
     const id = req.params.id
 
     try {
         const getCarreras = await carreraSchema.findById(id);
 
-        if(!getCarreras){
+        if (!getCarreras) {
             return res.status(404).json({
                 msg: 'No se encontraron resultados'
             })
